ec8ba89f0434e8fe7bff0cdb7ed3782e522e4376
Lvova Alina <lvova.aline@gmail.com>
2023-03-19 00:42:24 +0400

    feature: add auto year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,19 @@
+
+import {useState, useEffect} from 'react';
+
 function Footer(){
+    const [year, setYear] = useState(new Date().getFullYear());
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setYear(new Date().getFullYear());
+      }, 1000 * 60 * 60 * 24); // Обновляем год каждые 24 часа
+      return () => clearInterval(interval);
+    }, []);
+
     return(
         <footer className="footer">
-            <p className="footer__copyright">&#169; 2023 Mesto Russia</p>
+            <p className="footer__copyright">&#169; {year} Mesto Russia</p>
         </footer>
     )
 }
