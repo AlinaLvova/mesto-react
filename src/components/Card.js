eca51f68bbe589e73119a36e9c3cd5d112d48a46
Lvova Alina <lvova.aline@gmail.com>
2023-03-19 00:42:52 +0400

    feature: add cards

---
+++ src/components/Card.js
@@ -0,0 +1,48 @@
+import {useState} from 'react';
+import image_404 from './../images/404.webp';
+
+function Card(card) {
+  const [imageError, setImageError] = useState(false);
+
+  const handleImageError = () => {
+    setImageError(true);
+  };
+
+  return (
+    <li className="card" key={card.id}>
+      <article>
+        <button
+          className={`card__delete link ${card.isOwner ? 'card__delete_active' : ''}`}
+          aria-label="to-delete-card"
+        ></button>
+        {imageError ? (
+          <img 
+          className="card__image" 
+          src={image_404} 
+          alt="image not found" 
+          />
+        ) : (
+          <img
+            className="card__image"
+            src={card.link}
+            onError={handleImageError}
+            alt={card.name}
+          />
+        )}
+        <div className="card__footer">
+          <h2 className="card__title">{card.name}</h2>
+          <div className="card__like-container">
+            <button
+              className={`card__like link ${card.isLiked ? 'card__like_active' : ''}`}
+              type="button"
+              aria-label="liked"
+            ></button>
+            <h4 className="card__like-counter">{card.likes.length > 0 ? card.likes.length : ""}</h4>
+          </div>
+        </div>
+      </article>
+    </li>
+  );
+}
+
+export default Card;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,30 +1,54 @@
 import {useState, useEffect} from 'react';
 import api from '../utils/Api';
+import Card from './Card';
 
 function Main(props) {
   const [userName, setUserName] = useState();
   const [userAbout , setUserAbout] = useState();
   const [userAvatar, setUserAvatar] = useState();
+  const [cards, setCards] = useState([]);
 
-   useEffect(() => {
-     Promise.all([api.getUserInfo(), api.getCardList()])
-       .then(([infoData, cardListData]) => {
+  useEffect(() => {
+     api.getUserInfo()
+       .then(infoData => {
          setUserName(infoData.name);
          setUserAvatar(infoData.avatar);
          setUserAbout(infoData.about);
+         const idOwner = infoData._id;
+         console.log(idOwner);
+         api.getCardList()
+         .then(cardListData => {
+            setCards(cardListData.map((card) => ({
+                id: card._id,
+                name: card.name,
+                link: card.link,
+                likes: card.likes,
+                owner: card.owner,
+                isOwner: idOwner == card.owner._id,
+                isLiked: card.likes.some(like => like._id == idOwner)
+             })));
+             console.log(cards);
+         }) 
+         .catch((error) => {
+            console.log(error.message);
+          });
+    
        })
        .catch((error) => {
          console.log(error.message);
        });
-   }, []);
+     
+  }, []);
 
   return (
     <main className="content">
       <section className="profile">
         <div className="profile__info-group">
           <div className="profile__avatar-container">
-            <img className="profile__avatar" 
-                 style={{ backgroundImage: `url(${userAvatar})` }}
+            <img
+              className="profile__avatar"
+              style={{ backgroundImage: `url(${userAvatar})` }}
+              alt=""
             />
             <button
               className="profile__avatar-button"
@@ -55,7 +79,12 @@ function Main(props) {
       </section>
 
       <section className="gallery">
-        <ul className="gallery__list"></ul>
+        <ul className="gallery__list">
+          {cards.map((card) => 
+            <Card name={card.name} link={card.link} key={card.id} likes={card.likes} isOwner={card.isOwner} isLiked={card.isLiked}/>
+          )
+          }
+        </ul>
       </section>
     </main>
   );
